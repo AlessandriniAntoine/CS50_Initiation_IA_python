ef4c93cb50b5867926bdf77bf51620d6f9514bea
ALESSANDRINI <antoine.alessandrini@centrale.centralelille.fr>
2022-06-14 23:17:40 +0200

    Code Learning project 1 final version

--- Learning/project/shopping/shopping.py
+++ Learning/project/shopping/shopping.py
@@ -31,6 +31,25 @@ def main():
     print(f"True Negative Rate: {100 * specificity:.2f}%")
 
 
+def month(name):
+    """
+    return 0 for January, 1 for February, 2 for March, etc. up to 11 for December
+    """
+    list = ["Jan","Feb","Mar","Apr","May","June","Jul","Aug","Sep","Oct","Nov","Dec"]
+    return list.index(name)
+
+def visitortype(value):
+    """
+    return 1 for returning visitors and 0 for non-returning visitors
+    """
+    return 1 if value == "Returning_Visitor" else 0
+
+def weekend(value):
+    """
+    return 1 if the user visited on a weekend and 0 otherwise.
+    """
+    return int(value == "True")
+
 def load_data(filename):
     """
     Load shopping data from a CSV file `filename` and convert into a list of
@@ -38,36 +57,49 @@ def load_data(filename):
 
     evidence should be a list of lists, where each list contains the
     following values, in order:
-        - Administrative, an integer
-        - Administrative_Duration, a floating point number
-        - Informational, an integer
-        - Informational_Duration, a floating point number
-        - ProductRelated, an integer
-        - ProductRelated_Duration, a floating point number
-        - BounceRates, a floating point number
-        - ExitRates, a floating point number
-        - PageValues, a floating point number
-        - SpecialDay, a floating point number
-        - Month, an index from 0 (January) to 11 (December)
-        - OperatingSystems, an integer
-        - Browser, an integer
-        - Region, an integer
-        - TrafficType, an integer
-        - VisitorType, an integer 0 (not returning) or 1 (returning)
-        - Weekend, an integer 0 (if false) or 1 (if true)
+        - Administrative, an integer 
+        - Administrative_Duration, a floating point number 
+        - Informational, an integer 
+        - Informational_Duration, a floating point number 
+        - ProductRelated, an integer 
+        - ProductRelated_Duration, a floating point number 
+        - BounceRates, a floating point number 
+        - ExitRates, a floating point number 
+        - PageValues, a floating point number 
+        - SpecialDay, a floating point number 
+        - Month, an index from 0 (January) to (December) 
+        - OperatingSystems, an integer 
+        - Browser, an integer 
+        - Region, an integer 
+        - TrafficType, an integer 
+        - VisitorType, an integer 0 (not returning) or 1 (returning) 
+        - Weekend, an integer 0 (if false) or 1 (if true) 
 
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
-
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader) # pass the header
+        evidence = []
+        labels = []
+        for row in reader :
+            a = []
+            for i in range(3):
+                a.extend([int(row[2*i]),float(row[2*i+1])])
+            a += [float(cell) for cell in row[6:10]] + [month(row[10])] + [int(cell) for cell in row[11:15]] + [visitortype(row[15]),weekend(row[16])]
+            evidence.append(a)
+            labels.append(int(row[-1] == "TRUE"))
+    return (evidence,labels)
 
 def train_model(evidence, labels):
     """
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    model = KNeighborsClassifier(n_neighbors=1)
+    model.fit(evidence,labels)
+    return model.fit(evidence,labels)
 
 
 def evaluate(labels, predictions):
@@ -85,8 +117,22 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    positiveTotal,negativeTotal = 0,0
+    positive,negative = 0,0
+    for i in range(len(labels)) :
+        if labels[i] == 1:
+            positiveTotal +=1
+            if predictions[i] == labels[i]:
+                positive +=1
+        else :
+            negativeTotal +=1
+            if predictions[i] == labels[i]:
+                negative +=1
+    return (positive/positiveTotal,negative/negativeTotal)
+
+
 
 
 if __name__ == "__main__":
     main()
+
